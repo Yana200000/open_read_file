e81b15585bcbaeeedd000fd68bcddb48a21d3ae8
Yana200000 <89149827+Yana200000@users.noreply.github.com>
2021-09-15 20:11:46 +0300

    Add files via upload

---
+++ open_and_read(1).py
@@ -0,0 +1,65 @@
+from pprint import pprint
+
+file = open("recipes3.txt", "w+")
+file.write("""Омлет
+3
+Яйцо | 2 | шт
+Молоко | 100 | мл
+Помидор | 2 | шт
+
+Утка по-пекински
+4
+Утка | 1 | шт
+Вода | 2 | л
+Мед | 3 | ст.л
+Соевый соус | 60 | мл
+
+Запеченный картофель
+3
+Картофель | 1 | кг
+Чеснок | 3 | зубч
+Сыр гауда | 100 | г
+
+Фахитос
+5
+Говядина | 500 | г
+Перец сладкий | 1 | шт
+Лаваш | 2 | шт
+Винный уксус | 1 | ст.л
+Помидор | 2 | шт""")
+
+file.close()
+
+
+def prepare_dict(file_name: str) -> dict:
+    result: dict = dict()
+
+    with open(file_name) as my_file:
+        for line in my_file:
+            dish_name = line.strip()
+            records_quantity = int(my_file.readline())
+            ingredients_list = []
+            for ingredients in range(records_quantity):
+                ingredient_name, quantity, measure = my_file.readline().split('|')
+                ingredients_list.append(
+                    {"ingredient_name": ingredient_name, "quantity": int(quantity), "measure": measure.replace('\n', '')}
+                )
+            result[dish_name] = ingredients_list
+            my_file.readline()
+        return result
+
+
+cook_book = prepare_dict("recipes3.txt")
+#pprint(cook_book)
+
+def get_shop_list_by_dishes(dishes, person_count):
+    recipes = {}
+    for dish in dishes:
+        for i in range(0, len(cook_book[dish])):
+            recipes['quantity'] = cook_book[dish][i]['quantity'] * person_count
+            recipes['measure'] = cook_book[dish][i]['measure']
+            recipes = {cook_book[dish][i]['ingredient_name']: {'measure': recipes['measure'], 'quantity': recipes['quantity']}}
+            print(recipes)
+
+print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+
---
+++ open_read_file2.py
@@ -0,0 +1,57 @@
+import os
+from typing import Dict, Tuple, List
+
+file1 = open("1.txt", "w")
+file1.write("""Начальник  полиции
+лично позвонил в шестнадцатый участок. А спустя  одну минуту тридцать секунд
+в дежурке и других комнатах нижнего этажа раздались звонки
+     Когда Иенсен  --  комиссар  шестнадцатого  участка --  вышел  из своего
+кабинета,  звонки еще  не смолкли. Иенсен был мужчина средних лет,  обычного
+сложения, с лицом плоским и невыразительным. На последней ступеньке винтовой
+лестницы  он задержался  и  обвел взглядом помещение дежурки. Затем поправил
+галстук и проследовал к машине.""")
+file1.close()
+
+file2 = open("2.txt", "w")
+file2.write("""Тревога началась в тринадцать часов ноль две минуты. """)
+file2.close()
+
+file3 = open("3.txt", "w")
+file3.write("""В  это время  дня  машины текли сплошным  блестящим  потоком,  а  среди
+потока, будто  колонны из бетона  и стекла, высились  здания. Здесь,  в мире
+резких граней,  люди  на тротуарах  выглядели  несчастными и  неприкаянными.
+Одеты они были хорошо, но как-то удивительно походили друг на друга и все до
+одного спешили. Они шли нестройными  вереницами, широко разливались, завидев
+красный  светофор или  металлический  блеск кафе-автоматов.  Они непрестанно
+озирались по сторонам и теребили портфели и сумочки.
+     Полицейские  машины  с  включенными  сиренами  пробивались  сквозь  эту
+толчею.""")
+file3.close()
+
+import os
+
+os.chdir(r'C:\Users\User\Desktop\Нетология\hw 12.09')
+
+def merging_files():
+    list_files_txt =[]
+    my_dict = {}
+    for files in os.listdir(r"C:\Users\User\Desktop\Нетология\hw 12.09"):
+        if files.endswith(".txt"):
+            list_files_txt.append(files)
+            for i in list_files_txt[::-1]:
+                with open(i, encoding='cp1251') as files:
+                    list_strings = files.readlines()
+                    my_dict[i] = (len(list_strings), list_strings)
+    sort_dict ={}
+    list_for_sort = list(my_dict.items())
+    list_for_sort.sort(key=lambda i: i[1])
+    for i in list_for_sort:
+        sort_dict[i[0]] = i[1]
+
+    with open("file4.txt", "w", encoding='cp1251') as fail:
+        for key, value in sort_dict.items():
+            fail.write(f'{key}\n{value[0]}\n{"".join(value[1])}\n')
+
+
+
+print(merging_files())
